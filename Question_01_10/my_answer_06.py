bd69cda18c3dea19f4eafcc5cc4ddf57b5c317d6
MIURA Yasuyuki <kokosabu@gmail.com>
2019-02-22 20:58:38 +0900

    Q.6. 減色処理

--- Question_01_10/my_answer_06.py
+++ Question_01_10/my_answer_06.py
@@ -7,18 +7,18 @@ r = img[:,:,2]
 g = img[:,:,1]
 b = img[:,:,0]
 
-r[np.where((  0 <= r) &  (r <  63))] =  32
-r[np.where(( 64 <= r) &  (r < 127))] =  96
-r[np.where((127 <= r) &  (r < 191))] = 160
-r[np.where((191 <= r) &  (r < 256))] = 224
-g[np.where((  0 <= g) &  (g <  63))] =  32
-g[np.where(( 64 <= g) &  (g < 127))] =  96
-g[np.where((127 <= g) &  (g < 191))] = 160
-g[np.where((191 <= g) &  (g < 256))] = 224
-b[np.where((  0 <= b) &  (b <  63))] =  32
-b[np.where(( 64 <= b) &  (b < 127))] =  96
-b[np.where((127 <= b) &  (b < 191))] = 160
-b[np.where((191 <= b) &  (b < 256))] = 224
+r[np.where((  0 <= r) & (r <  64))] =  32
+r[np.where(( 64 <= r) & (r < 128))] =  96
+r[np.where((128 <= r) & (r < 192))] = 160
+r[np.where((192 <= r) & (r < 256))] = 224
+g[np.where((  0 <= g) & (g <  64))] =  32
+g[np.where(( 64 <= g) & (g < 128))] =  96
+g[np.where((128 <= g) & (g < 192))] = 160
+g[np.where((192 <= g) & (g < 256))] = 224
+b[np.where((  0 <= b) & (b <  64))] =  32
+b[np.where(( 64 <= b) & (b < 128))] =  96
+b[np.where((128 <= b) & (b < 192))] = 160
+b[np.where((192 <= b) & (b < 256))] = 224
 
 img[:,:,2] = r
 img[:,:,1] = g
