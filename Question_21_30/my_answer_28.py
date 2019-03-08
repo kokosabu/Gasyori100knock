1ef752f879a9d212fd4f70ac8c11ba3ea0842aa1
MIURA Yasuyuki <kokosabu@gmail.com>
2019-03-08 21:14:49 +0900

    Q.28. アフィン変換(平行移動)#2

--- Question_21_30/my_answer_28.py
+++ Question_21_30/my_answer_28.py
@@ -4,17 +4,25 @@ import numpy as np
 img = cv2.imread("imori.jpg").astype(np.float)
 H, W, C = img.shape
 
-out = np.zeros((H, W, C), dtype=np.float)
+a = 1
+b = 0
+c = 0
+d = 1
+tx = 30
+ty = -30
 
-A = np.array([[1, 0,  30],
-              [0, 1, -30],
-              [0, 0,   1]])
+y = np.arange(H).repeat(W).reshape(W, -1)
+x = np.tile(np.arange(W), (H, 1))
 
-for y in range(H):
-    for x in range(W):
-        (x_, y_, _) = np.matmul(A, np.array([x, y, 1]))
-        if 0 <= y_ and y_ < H and 0 <= x_ and x_ < W:
-            out[y_, x_] = img[y, x]
+out = np.zeros((H+1, W+1, C), dtype=np.float)
+
+x_new = a * x + b * y + tx
+y_new = c * x + d * y + ty
+
+x_new = np.minimum(np.maximum(x_new, 0), W).astype(np.int)
+y_new = np.minimum(np.maximum(y_new, 0), H).astype(np.int)
+
+out[y_new, x_new] = img[y, x]
+out = out[:H, :W]
 
 cv2.imwrite("my_answer_28.jpg", out.astype(np.uint8))
-
