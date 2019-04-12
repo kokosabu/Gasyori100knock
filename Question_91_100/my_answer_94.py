0020873ae36d9e1e46ce7f9c59bb6c8f25503acd
MIURA Yasuyuki <kokosabu@gmail.com>
2019-04-12 20:18:26 +0900

    Q.94. 機械学習の学習データの用意 (Step.2) ランダムクラッピング

---
+++ Question_91_100/my_answer_94.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+def iou(a, b):
+    start_x = np.max((a[0], b[0]))
+    start_y = np.max((a[1], b[1]))
+    end_x = np.min((a[2], b[2]))
+    end_y = np.min((a[3], b[3]))
+    w = end_x - start_x
+    h = end_y - start_y
+    if w < 0:
+        w = 0
+    if h < 0:
+        h = 0
+
+    R1 = (a[2]-a[0]) * (a[3]-a[1])
+    R2 = (b[2]-b[0]) * (b[3]-b[1])
+    Rol = w * h
+
+    return Rol / (R1 + R2 - Rol)
+
+#a = np.array((50, 50, 150, 150), dtype=np.float32)
+#b = np.array((60, 60, 170, 160), dtype=np.float32)
+#print(iou(a, b))
+
+img = cv2.imread("imori_1.jpg").astype(np.float)
+H, W, C = img.shape
+
+np.random.seed(0)
+gt = np.array((47, 41, 129, 103), dtype=np.float32)
+
+out = img.copy()
+out = cv2.rectangle(out, (gt[0], gt[1]), (gt[2], gt[3]), (0, 255, 0), 1)
+
+labels = np.zeros((200), dtype=np.int)
+for i in range(200):
+    crop = np.zeros((4), dtype=np.float32)
+    crop[0] = np.random.randint(W-60)
+    crop[1] = np.random.randint(H-60)
+    crop[2] = crop[0] + 60
+    crop[3] = crop[1] + 60
+    if iou(crop, gt) >= 0.5:
+        labels[i] = 1
+        color = (0, 0, 255)
+    else:
+        color = (255, 0, 0)
+    out = cv2.rectangle(out, (crop[0], crop[1]), (crop[2], crop[3]), color, 1)
+
+cv2.imwrite("my_answer_94.jpg", out.astype(np.uint8))
